13978ff5fda02fe6218095227418b11e8af621df
Daniyar.Al <alpuspaev.daniyar@gmail.com>
2018-12-14 14:19:21 +0600

    created UsersController

---
+++ StopoverAdminPanel/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using StopoverAdminPanel.UserModels;
+
+namespace StopoverAdminPanel.Controllers
+{
+	public class UsersController : Controller
+	{
+		public async Task<ActionResult> Register(RegisterUserModel model)
+		{
+			if (ModelState.IsValid)
+			{
+			}
+			return View(model);
+		}
+	}
+}
